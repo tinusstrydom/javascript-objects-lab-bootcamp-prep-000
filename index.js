5fe1a03a9fbd467e00b58d4f5fc53deb0ead618e
Tinus Strydom <tinuss@loans-inc.co.za>
2019-06-10 09:27:13 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,18 @@
+var recipes = {salt : "one pinch"};
+function updateObjectWithKeyAndValue(obj, key, value){
+	return Object.assign({}, obj,{[key]: value});
+}
+function destructivelyUpdateObjectWithKeyAndValue(obj, key, value){
+  obj[key] = value;
+  return obj;
+}
+function deleteFromObjectByKey(obj, key){
+  var newObj = Object.assign({}, obj);
+  delete newObj.key;
+  return newObj;
+}
+function deleteFromObjectByKey(obj, key){
+  var newObj = Object.assign({}, obj);
+  delete newObj[key];
+  return newObj;
+}
